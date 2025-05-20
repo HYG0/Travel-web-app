cac7a15e53fd30911dc3688ae719cf9af934d71a
Egor <tveritines@gmail.com>
2025-05-20 21:47:41 +0300

    Lint routes.py

--- app/routes.py
+++ app/routes.py
@@ -146,32 +146,31 @@ def register_routes(app):
 
         return jsonify(results)
 
-@app.route('/register', methods = ['GET', 'POST']) # связывает адрес с функцией
+    @app.route('/register', methods = ['GET', 'POST']) # связывает адрес с функцией
+    def register():
+        form = RegistrationForm() # экземпляр формы регистрации
+        if form.validate_on_submit():
+            # проверка на уже существуюшего пользователя
+            user = User.query.filter_by(username=form.username.data).first() # поиск пользователя с таким же именем в БД
+            if user:
+                flash("Это имя пользователя уже занято.", "danger")
+                return redirect(url_for("register"))
+            user = User.query.filter_by(email=form.email.data).first()
 
-def register():
-  form = RegistrationForm() # экземпляр формы регистрации
-  if form.validate_on_submit():
-    # проверка на уже существуюшего пользователя
-    user = User.query.filter_by(username=form.username.data).first() # поиск пользователя с таким же именем в БД
-    if user:
-      flash("Это имя пользователя уже занято.", "danger")
-      return redirect(url_for("register"))
-    user = User.query.filter_by(email=form.email.data).first()
+            if user:
+                flash("Этот email уже используется.", "danger")
+                return redirect(url_for("register"))
+            
 
-    if user:
-      flash("Этот email уже используется.", "danger")
-      return redirect(url_for("register"))
-    
-
-    # создание нового пользователя
-    new_user = User(username=form.username.data, email=form.email.data)
-    new_user.set_password(form.password.data)
+            # создание нового пользователя
+            new_user = User(username=form.username.data, email=form.email.data)
+            new_user.set_password(form.password.data)
 
 
-    # сохранение в БД
-    db.session.add(new_user)
-    db.session.commit()
+            # сохранение в БД
+            db.session.add(new_user)
+            db.session.commit()
 
-    flash("Аккаунт успешно создан! Теперь можно войти.", "success")
-    return redirect(url_for("index")) # перенаправляет юзера на главную страницу
-  return render_template("register.html", form=form)
+            flash("Аккаунт успешно создан! Теперь можно войти.", "success")
+            return redirect(url_for("index")) # перенаправляет юзера на главную страницу
+        return render_template("register.html", form=form)
